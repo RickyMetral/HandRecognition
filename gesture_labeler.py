20fc9e4c2e25822101d294a3f0aa3416889ebe31
Ricardo Metral <ricardometral2005@gmail.com>
2025-06-29 19:47:12 -0400

    Added deldayed pictures, and batch deletion

--- gesture_labeler.py
+++ gesture_labeler.py
@@ -9,6 +9,7 @@ import torchvision.transforms as T
 from PIL import Image
 import numpy as np
 
+#TODO Add a way to delete the previous batch of pics
 
 #List of transforms to be randomly applied to images
 transform = T.Compose([
@@ -26,8 +27,29 @@ transform = T.Compose([
     ], p=0.2),
     T.RandomVerticalFlip(p=0.3),
     T.RandomHorizontalFlip(p=0.3),
-    T.RandomGrayscale(p=0.1),   
+    T.RandomGrayscale(p=0.3),   
 ])
+
+
+def draw_landmarks(image, detected_image):
+    if detected_image.multi_hand_landmarks:
+         for landmark in detected_image.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, landmark, mp_hands.HAND_CONNECTIONS)
+
+
+def recv_frame(stream):
+    ret, frame = stream.read()
+    image = cv2.flip(frame, 1)
+    return (image, ret, )
+
+def get_frame_num(save_dir):
+    #Updates iter_count to the current amount of photos in the current directory
+    files = os.listdir(save_dir)
+    if len(files):
+        pattern =  re.compile(r'(\d+)')
+        files = sorted(files, key = lambda file: int(pattern.search(file).group(1)))
+        return int(pattern.search(files[-1]).group(1))+1
+    return 0
 
 #Converts ndarry to pil img
 def cv2_to_pil(cv_img):
@@ -43,17 +65,21 @@ mp_hands = mp.solutions.hands
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-d", "--directory", type=str, required = True,
+    ap.add_argument("-f", "--folder", type=str, required = True,
         help="Directory name to save images. Will create a directory if not currently existing. Will be inside train_images")# EX: python gesture_labeler.py -d five -> train_images/five
     ap.add_argument("-s", "--save_count", default=10, type=int,
         help="Changes how many frames will be saved into a sequence")
     ap.add_argument("-t", "--transform", default=True, type=bool,
         help="Boolean to determine whether or not to apply transformations to images before saving them" )
+    ap.add_argument("-d", "--delay", default = 0, type=int,
+        help="Adds a time delay to before taking capturing the image in seconds")
 
     iter_count = 0 # Keeps track of how many images have alr been saved in this directory
+    capture_requested = False
+    capture_time = -1
     args = vars(ap.parse_args())
     save_frame_count = args["save_count"]
-    save_dir = os.path.join("train_images", args["directory"])
+    save_dir = os.path.join("train_images", args["folder"])
     vs = cv2.VideoCapture(0)
 
     #Opening Webcam and letting it warm up
@@ -65,40 +91,52 @@ def main():
     #Creates the labeled directory if not already made
     if  not os.path.isdir(save_dir):
         os.mkdir(save_dir)
-    files = os.listdir(save_dir)
-    #Updates iter_count to the current amount of photos in the current directory
-    if len(files):
-        match = re.search(r'(\d+)', files[-1])
-        iter_count = int(match.group(1))+1
 
-    with mp_hands.Hands(min_detection_confidence=0.8,min_tracking_confidence=0.5) as hands:
+    iter_count = get_frame_num(save_dir)
+
+    with mp_hands.Hands(min_detection_confidence=0.8,min_tracking_confidence=0.5, max_num_hands=1) as hands:
         while True:
-            ret, frame = vs.read()
-            image = cv2.flip(frame, 1)
+            image, ret = recv_frame(vs)
+            #If frame was not returned
             if not ret:
                 break
             rgb_frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             key = cv2.waitKey(1)
-    
             detected_image = hands.process(rgb_frame)
-            if detected_image.multi_hand_landmarks:
-                 for landmark in detected_image.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(image, landmark, mp_hands.HAND_CONNECTIONS)
+            draw_landmarks(image, detected_image)
+    
             #Saves frames when "C" is pressed
             if key == ord("c"):
-                #Saving all the frames selected 
+                capture_requested = True
+                #Accounting for delay 
+                if args["delay"]:
+                    capture_time = time.time() + args["delay"] 
+                else:  
+                    capture_time = 0
+
+            if capture_requested and (capture_time - time.time()) <= 0:
+                capture_requested = False
                 pil_img = cv2_to_pil(image)
                 for i in range(save_frame_count):
                     #Saves the first image without any transformations and if transform is false, they are also not applied
                     if i == 0 or not args["transform"]:
-                        cv2.imwrite(f"{save_dir}\\{args['directory']}-frame{iter_count}.jpg", image)
+                        cv2.imwrite(f"{save_dir}\\{args['folder']}-frame{iter_count}.jpg", image)
+                        print(f"Saved {args['folder']}-frame{iter_count}.jpg")
                         iter_count += 1
-                        pass
+                        continue
+
+                    #Applies transformations and saves this iamge
                     pil_img = applyTransforms(pil_img, transform)
-                    cv2.imwrite(f"{save_dir}\\{args['directory']}-frame{iter_count}.jpg", np.array(pil_img))
-                    # augmented_image.save(f"{save_dir}\\{args['directory']}-frame{iter_count}.jpg")
+                    cv2.imwrite(f"{save_dir}\\{args['folder']}-frame{iter_count}.jpg", np.array(pil_img))
+                    print(f"Saved {args['folder']}-frame{iter_count}.jpg")
                     iter_count += 1
 
+            if key == ord("d"):
+                for i in range(save_frame_count):
+                    iter_count -= 1
+                    os.remove(f"{save_dir}\\{args['folder']}-frame{iter_count}.jpg")
+                    print(f"Deleted {args['folder']}-frame{iter_count}.jpg")
+                
             elif key == ord("q"):
                 #Removing the directory if no images were saved
                 if not os.listdir(save_dir):
